1e4940e38be4ad222b7a4305c07dae471e4bf1c7
tonijiny8 <tonijinyang@gmail.com>
2026-02-06 05:40:46 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our', 'his'];
+  let adj = ['great', 'big', 'amazing' ];
+  let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
+  
+  for(let i = 0; i < pronoun.length; i++) {
+    for(let j = 0; j < adj.length; j++) {
+      for(let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
+      }
+    }
+  }
